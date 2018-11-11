be3c1bc06bd98c85256b31628f7ae4bccf4e28b0
Jacky Lo <lojacky123@gmail.com>
2018-11-11 16:48:01 -0500

    add basic functionality of start screen

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour {
+    public void PlayMap1(){
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void PlayMap2()
+    {
+
+    }
+    public void PlayMap3()
+    {
+
+    }
+    public void PlayMap4()
+    {
+
+    }
+    public void PlayMap5()
+    {
+
+    }
+    public void PlayMap6()
+    {
+
+    }
+}
