69352cdf0605e3c2c3256debedb5e5c3e4187d0c
ChristopherWennendy <christopherwennendy@gmail.com>
2018-11-11 16:47:55 -0500

    Script Skeleton
    Player Methods and Stuff for controls, and bombs

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -2,18 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class GameSystem : MonoBehaviour {
 
-	public GameObject allyPlayer;
-	public GameObject axisPlayer;
+	public GameObject allyPlayerPrefab;
+	public GameObject axisPlayerPrefab;
+	private GameObject allyPlayerObject;
+	private GameObject axisPlayerObject;
+	private Player allyPlayer;
+	private Player axisPlayer;
+	private int minimumHeight = 0;
+	public Vector3 allyPlayerStartPosition;
+	public Vector3 axisPlayerStartPosition;
 
 	// Use this for initialization
 	void Start () {
-		
+		allyPlayerObject = Instantiate (allyPlayerPrefab);
+		axisPlayerObject = Instantiate (axisPlayerPrefab);
+		allyPlayer = allyPlayerObject.GetComponent<Player> ();
+		axisPlayer = axisPlayerObject.GetComponent<Player> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Check if any Player fell off
+		if (allyPlayerObject.transform.position.y < minimumHeight){
+			fellOff (allyPlayerObject);
+		}
+		if (axisPlayerObject.transform.position.y < minimumHeight){
+			fellOff (axisPlayerObject);
+		}
+
+		//Ally Player Movement
+		if (Input.GetKey (KeyCode.W)) {
+			allyPlayer.moveUp();
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			allyPlayer.moveLeft();
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			allyPlayer.moveRight();
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			allyPlayer.moveDown();
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			allyPlayer.grabBomb();
+		}
+		if (Input.GetKey (KeyCode.R)) {
+			allyPlayer.jump();
+		}
+
+		//Axis Player Movement
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			axisPlayer.moveUp();
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			axisPlayer.moveLeft();
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			axisPlayer.moveRight();
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			axisPlayer.moveDown();
+		}
+		if (Input.GetKey (KeyCode.Period)) {
+			allyPlayer.grabBomb();
+		}
+		if (Input.GetKey (KeyCode.Slash)) {
+			allyPlayer.jump ();
+		}
+
+	}
 		
+	private void fellOff(GameObject player){
+
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	private int Health;
+	private int bombCharge;
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		Health = 5;
+		rb = GetComponent<Rigidbody>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		//Increase Bomb Charges
+		if (bombCharge > 0 && bombCharge < 100)
+			bombCharge += 1;	
+	}
+
+	//Grab Bomb, and if already holding then throw
+	public bool grabBomb(){
+		if (bombCharge != 0) {
+			throwBomb ();
+			return false;
+		} else {
+			bombCharge = 1;
+			return true;
+		}
+	}
+	private void throwBomb (){
 		
 	}
+
+	//Movement
+	public void moveUp(){
+
+	}
+	public void moveDown(){
+
+	}
+	public void moveLeft(){
+
+	}
+	public void moveRight(){
+
+	}
+	public void jump(){
+
+	}
+
 }
