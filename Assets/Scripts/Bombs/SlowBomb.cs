c8fee104cdd29888ab89e58fd850bc7958204f6d
ChristopherWennendy <christopherwennendy@gmail.com>
2018-11-29 21:32:36 -0500

    Rest of the prefabs and scripts
    finally

---
+++ Assets/Scripts/Bombs/SlowBomb.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowBomb : Bomb {
+
+	protected override void explode(){
+		Destroy(gameObject);
+	}
+
+}
