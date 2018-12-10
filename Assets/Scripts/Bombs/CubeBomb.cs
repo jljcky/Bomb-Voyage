4a583563bfb52ead1c6a7db5f46cb9942edbd5e4
ChristopherWennendy <christopherwennendy@gmail.com>
2018-12-10 16:31:51 -0500

    Terrain
    Terrain Bomb

---
+++ Assets/Scripts/Bombs/CubeBomb.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBomb : Bomb {
+	protected override void Update()
+	{
+		base.Update();
+	}
+
+	protected override void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.layer == 10){
+			explode ();
+		}
+	}
+
+	protected override void explode(){
+		Collider[] collidersNearby = Physics.OverlapSphere(transform.position, 8f * bombCharge);
+		foreach (Collider c in collidersNearby)
+		{
+			if (c.gameObject.layer == 9)
+			{
+				c.GetComponent<Player>().GetHit(bombCharge, transform.position);
+			}
+		}
+		GameObject e = Instantiate(explosion, transform.position, transform.rotation);
+		e.transform.localScale = Vector3.one * bombCharge;
+
+		GameObject newCube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		newCube.transform.position = transform.position;
+		newCube.transform.rotation = transform.rotation;
+		newCube.transform.localScale = Vector3.one * 5F * bombCharge;
+		newCube.transform.position = transform.position + new Vector3 (0, (newCube.transform.localScale.y / 2f) - 1, 0);
+		newCube.gameObject.layer = 10;
+		Destroy(gameObject);
+	}
+}
+
