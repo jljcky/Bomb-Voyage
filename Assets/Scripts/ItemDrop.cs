ad3df091c4933b071614fcd50cb3aff8cdd589c1
Jacky Lo <lojacky123@gmail.com>
2018-12-11 20:44:18 -0500

    fixed itemdrop script a little bit, needs more refinement

--- Assets/Scripts/ItemDrop.cs
+++ Assets/Scripts/ItemDrop.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDrop : MonoBehaviour {
-    public GameObject[] items;
+public class ItemDrop : MonoBehaviour
+{
+    [System.Serializable]
+    public class ItemDropData
+    {
+        public GameObject item;
+        public float rate;
+    }
+
+    public ItemDropData[] items;
     //private Vector3 center;
     private float itemDropTime = 20f;
     private float itemCDTime = 0f;
@@ -13,7 +21,24 @@ public class ItemDrop : MonoBehaviour {
         transform.RotateAround(Vector3.zero, Vector3.up, 5f*Time.deltaTime);
         if (itemCDTime >= itemDropTime)
         {
-            Instantiate(items[Random.Range(0, items.Length)], transform.position, transform.rotation);
+            float sum = 0f;
+            foreach (ItemDropData d in items)
+            {
+                sum += d.rate;
+            }
+            float rate = Random.Range(0,sum+1);
+            GameObject item = null;
+            float check = 0f;
+            foreach (ItemDropData d in items)
+            {
+                check += d.rate;
+                if (rate <= check)
+                {
+                    item = d.item;
+                    break;
+                }
+            }
+            Instantiate(item, transform.position, transform.rotation);
             itemCDTime = 0f;
         }
         else
