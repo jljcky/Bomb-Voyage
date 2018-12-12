471b1d2bfac119861f966ad2bda8a5a4911e79f5
Jacky Lo <lojacky123@gmail.com>
2018-12-12 14:53:56 -0500

    item drops random location i think

--- Assets/Scripts/ItemDrop.cs
+++ Assets/Scripts/ItemDrop.cs
@@ -12,13 +12,14 @@ public class ItemDrop : MonoBehaviour
     }
 
     public ItemDropData[] items;
+    public MeshRenderer[] dropLocations;
     //private Vector3 center;
     private float itemDropTime = 20f;
     private float itemCDTime = 0f;
 	
 	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(Vector3.zero, Vector3.up, 5f*Time.deltaTime);
+        //transform.RotateAround(Vector3.zero, Vector3.up, 5f*Time.deltaTime);
         if (itemCDTime >= itemDropTime)
         {
             float sum = 0f;
@@ -38,7 +39,15 @@ public class ItemDrop : MonoBehaviour
                     break;
                 }
             }
-            Instantiate(item, transform.position, transform.rotation);
+            MeshRenderer placeToDrop = dropLocations[(int)Random.Range(0f, dropLocations.Length)];
+            Bounds bounds = placeToDrop.bounds;
+            float minX = bounds.center.x - bounds.size.x / 2f;
+            float maxX = bounds.center.x + bounds.size.x / 2f;
+            float minZ = bounds.center.z - bounds.size.z / 2f;
+            float maxZ = bounds.center.z + bounds.size.z / 2f;
+            float randomX = Random.Range(minX, maxX);
+            float randomZ = Random.Range(minZ, maxZ);
+            Instantiate(item, new Vector3(randomX, 50f, randomZ), Quaternion.identity);
             itemCDTime = 0f;
         }
         else
