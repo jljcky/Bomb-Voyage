5c0180b399f32d08fda0bcaf93f86a6e65f36769
Jacky Lo <lojacky123@gmail.com>
2018-12-27 20:44:16 -0500

    add camera re-centering on players
    smooth camera

---
+++ Assets/Scripts/CameraCenter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCenter : MonoBehaviour {
+
+    public GameObject allyPlayer;
+    public GameObject axisPlayer;
+    private Vector3 startPos;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        startPos = transform.position;
+        velocity = Vector3.zero;
+    }
+
+    void Update()
+    {
+        Vector3 pos1 = allyPlayer.transform.position;
+        Vector3 pos2 = axisPlayer.transform.position;
+        pos1.y = 0f;
+        pos2.z = 0f;
+        Vector3 midpoint = pos1 + (pos2 - pos1) / 2f;
+        transform.position = Vector3.SmoothDamp(transform.position, startPos+midpoint, ref velocity, 10f);
+    }
+}
